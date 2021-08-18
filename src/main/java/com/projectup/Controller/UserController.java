c33d6604c4ed8e79f21f6d6a9e7c068c98e31db2
Juan Sebastian Calvo <jscalvo5@misena.edu.co>
2021-08-18 09:05:19 -0500

    se agregaron las librerias para hacer las validaciones pero no se han terminadoXD

--- src/main/java/com/projectup/Controller/UserController.java
+++ src/main/java/com/projectup/Controller/UserController.java
@@ -7,11 +7,13 @@ import com.projectup.beans.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 @SuppressWarnings("All")
 @Controller
@@ -42,8 +44,11 @@ public class UserController {
 
     // Método agregar un nuevo usuario
     @PostMapping("/registrar")
-    public String registrarUsario(User user,Model model){
+    public String registrarUsario(@Valid User user, BindingResult res, Model model){
         user  = userService.save(user);
+        if (res.hasErrors()){
+            return "register";
+        }
         if(user != null){
             System.out.println("Se registro exitosamente");
         }else{
