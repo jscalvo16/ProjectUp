91367067a6622729927fae6e738b8476ac7ad965
Nicolas Rosero <nicolass2310@gmail.com>
2021-09-06 10:14:32 -0500

    Grupos en curso

--- src/main/java/com/projectup/Controller/FichaController.java
+++ src/main/java/com/projectup/Controller/FichaController.java
@@ -2,6 +2,7 @@ package com.projectup.Controller;
 
 import com.projectup.Service.FichaService;
 import com.projectup.beans.Ficha;
+import com.projectup.beans.Grupo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +14,11 @@ import java.util.List;
 @SuppressWarnings("All")
 @Controller
 public class FichaController {
+
     @Autowired
     private FichaService fichser;
 
+    // Mostrar el listado de fichas
     @GetMapping("/fichas")
     public String listaFichas(Model model) {
         List<Ficha> listaFichas = fichser.fichaList();
@@ -24,12 +27,21 @@ public class FichaController {
         return "plantilla/plantilla";
     }
 
+    // Mostrar la información de la ficha seleccionada por id
     @GetMapping("/ficha/{id}")
     public String fichaId(@PathVariable("id") int id, Model model){
+        // Mostrar información de la ficha
         Ficha ficha=fichser.fichaById(id).get();
-        model.addAttribute("infoFicha",ficha);
-        model.addAttribute("grupos",ficha.getListGrupos());
-        model.addAttribute("lista",ficha.getListaPivoteF());
+        model.addAttribute("infoFicha", ficha);
+
+        // Mostrar información de los grupos de la ficha
+        model.addAttribute("grupos", ficha.getListGrupos());
+
+        // Mostar información de los integrantes de la ficha
+        model.addAttribute("lista", ficha.getListaPivoteF());
+
+        // Mostrar información para crear un grupo de proyecto
+        model.addAttribute("grupoInfo", new Grupo());
         return "fichas";
     }
 }
